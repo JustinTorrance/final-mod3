43ff50832dd2112c35b9cf8b4483e8f0cf7517dd
Justin Torrance <justin.torrance91@gmail.com>
2019-04-16 10:02:08 -0600

    Complete iteration 1

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,18 +2,18 @@ import React from 'react'
 
 export const Card = ({name, founded, seats, titles, coatOfArms, ancestralWeapons, words}) => {
 
-  const mappedSeats = seats.map(seat => <p>{seat}</p>)
-  const mappedTitles = titles.map(title => <p>{title}</p>)
-  const mappedAncestralWeapons = ancestralWeapons.map(weapon => <p>{weapon}</p>)
+  const mappedSeats = seats.map(seat => <li>{seat}</li>)
+  const mappedTitles = titles.map(title => <li>{title}</li>)
+  const mappedAncestralWeapons = ancestralWeapons.map(weapon => <li>{weapon}</li>)
 
   return(
     <article>
       <h3>{name}</h3>
       <p>Founded: {founded}</p>
-      <div>Seats: {mappedSeats}</div>
-      <div>Titles: {mappedTitles}</div>
+      <ul>Seats: {mappedSeats}</ul>
+      <ul>Titles: {mappedTitles}</ul>
       <p>Coat of Arms: {coatOfArms}</p>
-      <div>Ancestral Weapons: {mappedAncestralWeapons}</div>
+      <ul>Ancestral Weapons: {mappedAncestralWeapons}</ul>
       <p>Words: {words}</p>
     </article>
   )
